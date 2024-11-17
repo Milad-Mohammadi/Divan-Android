d2fdbdd443edcd08d281dc2ffe24e1de90629830
Milad Mohammadi <Mohammadi.Dev@gmail.com>
2024-11-17 18:27:15 +0330

    Added level to Props

--- Divan/src/main/java/ac/divan/data/remote/dto/menu/Props.kt
+++ Divan/src/main/java/ac/divan/data/remote/dto/menu/Props.kt
@@ -3,6 +3,7 @@ package ac.divan.data.remote.dto.menu
 data class Props(
     val text: String?,
     val type: String?,
+    val level: Int?,
     val data: Any? // TODO: This is PropsData, but sometimes comes as `"data": "[object Object]", handle it in code`
 ) {
     data class PropsData(
